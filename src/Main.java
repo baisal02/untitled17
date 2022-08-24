aa3a9829bbc60f838a8a617ecb76c12907862faa
Lenovo <altynbekuulubajsal@gmail.com>
2022-08-24 11:21:49 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,50 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Adyl adyl = new Adyl(1253456,"Adyl","Adylov",20,"adyl@gmail.com","Bishkek");
-        Asan asan = new Asan(1124223,"Asan","s",20,"sdaf@dfs","bishkek");
-        Uson uson =new Uson(1243224,"Uson","Usonov",19,"Uson@mailru","Tokmok");
-        Akyl akyl = new Akyl(4324322,"Akyl","Akylov",17,"akyl@gmal.com","Osh");
-        Ainur ainur = new Ainur(754343,"Ainur","Asanova",18,"ainur@gmail.com","Moscow");
-        People[]people = {adyl,asan,akyl,ainur,uson};
+        Adyl adyl = new Adyl(1253456, "Adyl", "Adylov", 20, "adyl@gmail.com", "Bishkek");
+        Asan asan = new Asan(1124223, "Asan", "s", 20, "sdaf@dfs", "bishkek");
+        Uson uson = new Uson(1243224, "Uson", "Usonov", 19, "Uson@mailru", "Tokmok");
+        Akyl akyl = new Akyl(4324322, "Akyl", "Akylov", 17, "akyl@gmal.com", "Osh");
+        Ainur ainur = new Ainur(754343, "Ainur", "Asanova", 18, "ainur@gmail.com", "Moscow");
+        People[] people = {adyl, asan, akyl, ainur, uson};
 
-        Adyl[]adyls = new Adyl[1];//size jonle 1 berip koidum, glavnyi people sizenynan chon je barabar bolso boldu
-        Asan[]asans = new Asan[1];
+        Adyl[] adyls = new Adyl[1];//size jonle 1 berip koidum, glavnyi people sizenynan chon je barabar bolso boldu
+        Asan[] asans = new Asan[1];
         Uson[] usons = new Uson[1];
-        Akyl[]akyls = new Akyl[1];
-        Ainur[]ainurs = new Ainur[1];
+        Akyl[] akyls = new Akyl[1];
+        Ainur[] ainurs = new Ainur[1];
 
-        int i = 0; //massicterdi tolturush uchun counterler
+        int i = 0; // <---  massivterdi tolturush uchun counterler
         int w = 0;
         int r = 0;
         int t = 0;
         int q = 0;
-        for (People p:
-             people) {
-            if(p instanceof Adyl){
-                adyls[i]=(Adyl)p;
-                i++;
+        for (People p :
+                people) {
+            if (p instanceof Adyl) {
+                adyls[i] = (Adyl) p;
+                i++;   //<--- bular bir indexke kaira kaira jany maani berbesh uchun jazyldy
             } else if (p instanceof Asan) {
-                asans[w]=(Asan)p;
+                asans[w] = (Asan) p;
                 w++;
-            } else if (p instanceof Uson ){
-                usons[r]=(Uson) p;
+            } else if (p instanceof Uson) {
+                usons[r] = (Uson) p;
+                r++;
             } else if (p instanceof Akyl) {
-                akyls[t]=(Akyl)p;
+                akyls[t] = (Akyl) p;
+                t++;
             } else if (p instanceof Ainur) {
-                ainurs[q]=(Ainur)p;
+                ainurs[q] = (Ainur) p;
+                q++;
             }
-
             for (People p1 :
-                 people) {
+                    people) {
                 System.out.println(p1);
                 p1.work();
                 p1.ToRelax();
                 p1.communications();
                 p1.timetable();
-                System.out.println('\n'+" ");
+                System.out.println('\n' + " ");
             }
         }
     }
